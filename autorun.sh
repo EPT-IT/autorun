2a4dbb2ce71787ef9b215b259a629cee10723dda
EPT-IT <100860981+EPT-IT@users.noreply.github.com>
2022-03-04 10:38:50 +0100

    Update autorun.sh
    Final????????

--- autorun.sh
+++ autorun.sh
@@ -1,18 +1,18 @@
 #This is a script to automate the process of creating a user with the correct flags for NTNU
 
 #This adds colors to the script
-BLACK='\033[0;30m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-WHITE='\033[0;37m'
+BLACK='\033[30m'
+RED='\033[31m'
+GREEN='\033[32m'
+ORANGE='\033[33m'
+BLUE='\033[34m'
+PURPLE='\033[35m'
+CYAN='\033[36m'
+WHITE='\033[37m'
 NC='\033[0m' # No Color
 
 #The line bellow shows colors, comment out once done
-#echo ${BLACK}BLACK ${RED}RED ${GREEN}GREEN ${ORANGE}ORANGE ${BLUE}BLUE ${PURPLE}PURPLE ${CYAN}CYAN ${WHITE}WHITE ${NC}NC
+echo ${BLACK}BLACK ${RED}RED ${GREEN}GREEN ${ORANGE}ORANGE ${BLUE}BLUE ${PURPLE}PURPLE ${CYAN}CYAN ${WHITE}WHITE ${NC}NC
 
 
 
@@ -39,25 +39,3 @@ else
 echo ${RED}No or invalid UID has been selected, UID had been skipped.${NC}
 fi
 echo ""
-
-# Add user with flags based on input
-if [[ $inputuid =~ ^[[:digit:]]+$ ]];then
-sudo useradd --create-home --user-group --groups adm,cdrom,sudo,dip,plugdev,lpadmin,lxd,sambashare --uid $inputuid --comment "$inputfullname" $username
-echo ${ORANGE}User ${GREEN}$username ${ORANGE}has been created for ${GREEN}$inputfullname ${ORANGE}with the UID ${GREEN}$inputuid${NC}
-else
-sudo useradd --create-home --user-group --groups adm,cdrom,sudo,dip,plugdev,lpadmin,lxd,sambashare --comment "$inputfullname" $username
-echo ${ORANGE}User ${GREEN}$username ${ORANGE}has been created for ${GREEN}$inputfullname ${ORANGE}without any ${RED}UID${NC}
-fi
-
-
-# Set password for user
-sudo passwd $username
-
-# Update
-sudo apt update
-sudo apt install net-tools
-sudo apt install htop
-sudo apt install gparted
-sudo apt install neofetch
-
-sudo apt upgrade
