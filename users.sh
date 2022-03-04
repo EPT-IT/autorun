7596e9dffbb6341af1ec890d9c08fdb842701dbf
EPT-IT <100860981+EPT-IT@users.noreply.github.com>
2022-03-04 14:37:40 +0100

    Update users.sh

--- users.sh
+++ users.sh
@@ -16,35 +16,35 @@ NC='\033[0m' # No Color
 
 
 # Ask the user for their username
-echo -e ${CYAN}Please input a Username:${NC}
+echo -e "${CYAN}Please input a Username:${NC}"
 read inputusername
 username=$(echo $inputusername | tr '[:upper:]' '[:lower:]')
-echo -e ${ORANGE}Username has been set to ${GREEN}$username
+echo -e "${ORANGE}Username has been set to ${GREEN}$username"
 echo ""
 
 # Ask the user for their name
-echo -e ${CYAN}Please input their full name:${NC}
+echo -e "${CYAN}Please input their full name:${NC}"
 read inputfullname
-echo -e ${ORANGE}Full name is ${GREEN}$inputfullname
+echo -e "${ORANGE}Full name is ${GREEN}$inputfullname"
 echo ""
 
 # Ask the user for their uid
-echo -e -n ${CYAN}Please input the UID:${NC}
+echo -e -n "${CYAN}Please input the UID:${NC}"
 read inputuid
 if [[ $inputuid =~ ^[[:digit:]]+$ ]];then
-echo -e ${ORANGE}The UID is ${GREEN}$inputuid ${NC}
+echo -e "${ORANGE}The UID is ${GREEN}$inputuid ${NC}"
 else
-echo -e ${RED}No or invalid UID has been selected, UID has been skipped.${NC}
+echo -e "${RED}No or invalid UID has been selected, UID has been skipped.${NC}"
 fi
 echo ""
 
 # Add user with flags based on input
 if [[ $inputuid =~ ^[[:digit:]]+$ ]];then
-sudo useradd --create-home --user-group --groups adm,cdrom,sudo,dip,plugdev,lpadmin,lxd,sambashare --uid $inputuid --comment "$inputfullname" $username
-echo -e ${ORANGE}User ${GREEN}$username ${ORANGE}has been created for ${GREEN}$inputfullname ${ORANGE}with the UID ${GREEN}$inputuid${NC}
+sudo useradd --create-home --user-group --groups adm,cdrom,sudo,dip,plugdev,lpadmin,lxd,sambashare --uid $inputuid --shell /bin/bash --comment "$inputfullname" $username
+echo -e "${ORANGE}User ${GREEN}$username ${ORANGE}has been created for ${GREEN}$inputfullname ${ORANGE}with the UID ${GREEN}$inputuid${NC}"
 else
-sudo useradd --create-home --user-group --groups adm,cdrom,sudo,dip,plugdev,lpadmin,lxd,sambashare --comment "$inputfullname" $username
-echo -e ${ORANGE}User ${GREEN}$username ${ORANGE}has been created for ${GREEN}$inputfullname ${ORANGE}without any ${RED}UID${NC}
+sudo useradd --create-home --user-group --groups adm,cdrom,sudo,dip,plugdev,lpadmin,lxd,sambashare --shell /bin/bash --comment "$inputfullname" $username
+echo -e "${ORANGE}User ${GREEN}$username ${ORANGE}has been created for ${GREEN}$inputfullname ${ORANGE}without any ${RED}UID${NC}"
 fi
 
 
@@ -52,10 +52,25 @@ fi
 sudo passwd $username
 
 # Update
-sudo apt update
-sudo apt install net-tools
-sudo apt install htop
-sudo apt install gparted
-sudo apt install neofetch
-# Upgrade
-sudo apt upgrade
+echo ""
+echo -e "${ORANGE}Would you like to update and install the carepackage?${NC}"
+read -r -p "[Y/n] " input
+ 
+case $input in
+      [yY][eE][sS]|[yY])
+            sudo apt update
+            sudo apt install net-tools
+            sudo apt install htop
+            sudo apt install gparted
+            sudo apt install neofetch
+            # Upgrade
+            sudo apt upgrade
+            ;;
+      [nN][oO]|[nN])
+            echo "You say No"
+            ;;
+      *)
+            echo "Invalid input..."
+            exit 1
+            ;;
+esac
