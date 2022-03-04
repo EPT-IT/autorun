1faaa86bf5a8113378eef9f743e101840370cd6c
EPT-IT <100860981+EPT-IT@users.noreply.github.com>
2022-03-04 08:24:12 +0100

    Update autorun.sh
    Added if else arguments

--- autorun.sh
+++ autorun.sh
@@ -2,20 +2,27 @@
 # Ask the user for their username
 echo Please input a Username:
 read inputusername
- 
+
 # Ask the user for their name
 echo Please input their full name:
 read inputfullname
- 
+
 # Ask the user for their uid
 echo -n Please input the UID:
 read inputuid
- 
-# Add user with flags and variables
+
+# Add user with flags based on input
+
+if [[ $inputuid =~ ^[[:digit:]]+$ ]];then
 sudo useradd --create-home --user-group --groups adm,cdrom,sudo,dip,plugdev,lpadmin,lxd,sambashare --uid $inputuid --comment "$inputfullname" $inputusername
 
-# Set password
-sudo psswd $inputusername
+else
+sudo useradd --create-home --user-group --groups adm,cdrom,sudo,dip,plugdev,lpadmin,lxd,sambashare --comment "$inputfullname" $inputusername
+fi
+
+
+# Set password for user
+sudo passwd $inputusername
 
 # Update
 sudo apt update
