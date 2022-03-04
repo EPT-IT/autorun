c157791ecce1f20d1a548cdcf3db16803e2a7378
EPT-IT <100860981+EPT-IT@users.noreply.github.com>
2022-03-04 10:36:08 +0100

    Update autorun.sh
    Fianl????

--- autorun.sh
+++ autorun.sh
@@ -1,32 +1,52 @@
 #This is a script to automate the process of creating a user with the correct flags for NTNU
 
+#This adds colors to the script
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+NC='\033[0m' # No Color
+
+#The line bellow shows colors, comment out once done
+echo ${BLACK}BLACK ${RED}RED ${GREEN}GREEN ${ORANGE}ORANGE ${BLUE}BLUE ${PURPLE}PURPLE ${CYAN}CYAN ${WHITE}WHITE ${NC}NC
+
+
+
+
 # Ask the user for their username
-echo Please input a Username:
+echo ${CYAN}Please input a Username:${NC}
 read inputusername
 username=$(echo $inputusername | tr '[:upper:]' '[:lower:]')
-echo Username has been set to $username
+echo ${ORANGE}Username has been set to ${GREEN}$username
+echo ""
 
 # Ask the user for their name
-echo Please input their full name:
+echo ${CYAN}Please input their full name:${NC}
 read inputfullname
-echo Full name is $inputfullname
+echo ${ORANGE}Full name is ${GREEN}$inputfullname
+echo ""
 
 # Ask the user for their uid
-echo -n Please input the UID:
+echo -n ${CYAN}Please input the UID:${NC}
 read inputuid
 if [[ $inputuid =~ ^[[:digit:]]+$ ]];then
-echo The UID is $inputuid
+echo ${ORANGE}The UID is ${GREEN}$inputuid ${NC}
 else
-echo No or invalid UID has been selected, UID had been skipped.
+echo ${RED}No or invalid UID has been selected, UID had been skipped.${NC}
 fi
+echo ""
 
 # Add user with flags based on input
 if [[ $inputuid =~ ^[[:digit:]]+$ ]];then
 sudo useradd --create-home --user-group --groups adm,cdrom,sudo,dip,plugdev,lpadmin,lxd,sambashare --uid $inputuid --comment "$inputfullname" $username
-echo User $username has been created for $inputfullname with the UID $inputuid
+echo ${ORANGE}User ${GREEN}$username ${ORANGE}has been created for ${GREEN}$inputfullname ${ORANGE}with the UID ${GREEN}$inputuid${NC}
 else
 sudo useradd --create-home --user-group --groups adm,cdrom,sudo,dip,plugdev,lpadmin,lxd,sambashare --comment "$inputfullname" $username
-echo User $username has been created for $inputfullname without any UID
+echo ${ORANGE}User ${GREEN}$username ${ORANGE}has been created for ${GREEN}$inputfullname ${ORANGE}without any ${RED}UID${NC}
 fi
 
 
@@ -35,4 +55,9 @@ sudo passwd $username
 
 # Update
 sudo apt update
+sudo apt install net-tools
+sudo apt install htop
+sudo apt install gparted
+sudo apt install neofetch
+
 sudo apt upgrade
